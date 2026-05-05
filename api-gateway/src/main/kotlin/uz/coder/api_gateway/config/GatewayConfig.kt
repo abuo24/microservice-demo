f495244d4c1031d33ee8d8053de8c8159f02f959
abuo24 <mryediniofficial9924@gmail.com>
2026-05-05 23:10:01 +0500

    add service discovery and registry

---
+++ api-gateway/src/main/kotlin/uz/coder/api_gateway/config/GatewayConfig.kt
@@ -0,0 +1,24 @@
+package uz.coder.api_gateway.config
+
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class GatewayConfig {
+
+    @Bean
+    fun routes(builder: RouteLocatorBuilder): RouteLocator = builder.routes()
+        .route("order-service") { r ->
+            r.path("/api/orders/**")
+                .filters { f -> f.addRequestHeader("X-Source", "api-gateway") }
+                .uri("lb://order-service")
+        }
+        .route("inventory-service") { r ->
+            r.path("/api/inventory/**")
+                .filters { f -> f.addRequestHeader("X-Source", "api-gateway") }
+                .uri("lb://inventory-service")
+        }
+        .build()
+}
