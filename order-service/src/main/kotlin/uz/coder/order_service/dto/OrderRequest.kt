a13ea9595e56e43cb9482f4128af3a5cf22f8621
abuo24 <mryediniofficial9924@gmail.com>
2026-05-07 21:53:23 +0500

    add distributed tracing. brave/zipkin

--- order-service/src/main/kotlin/uz/coder/order_service/dto/OrderRequest.kt
+++ order-service/src/main/kotlin/uz/coder/order_service/dto/OrderRequest.kt
@@ -6,10 +6,3 @@ data class OrderRequest(
     val customerId: String,
     val items: List<OrderItemRequest>
 )
-
-data class OrderItemRequest(
-    val productId: String,
-    val productName: String,
-    val quantity: Int,
-    val unitPrice: BigDecimal
-)
